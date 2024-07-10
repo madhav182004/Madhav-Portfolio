001dec1091c4465880d4ee12ce02fc6ba0017fbd
Madhav Mundhra <madhavmundhra221@gmail.com>
2024-07-10 09:40:13 +0530

    Updated my projects

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-// import { Tilt } from 'react-tilt';
-import { Tilt } from '/node_modules/.vite/deps/react-tilt.js';
+import { Tilt } from 'react-tilt';
+// import { Tilt } from '/node_modules/.vite/deps/react-tilt.js';
 import { motion } from 'framer-motion';
 import { styles } from '../styles';
 import { services } from '../constants';
